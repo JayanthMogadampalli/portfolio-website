02ae0ac1e73dc16d828ea37bd02686d9a7ec87b4
JayanthMogadampalli <jayu6766@gmail.com>
2021-03-31 00:48:05 +0530

    added posts section

--- src/App.js
+++ src/App.js
@@ -7,21 +7,20 @@ import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
 import Posts from './components/Posts';
-import Projects from './components/Projects';
 import { Row, Col, Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="mainDiv">
-        <Navbar expand="lg" className="navbr">
+        <Navbar collapseOnSelect expand="lg" className="navbr">
           <Navbar.Brand href="#home">Jayanth Mogadampalli</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
               <NavLink to="/" exact activeClassName="selected" className="ml-2 mr-2">Home</NavLink>
               <NavLink to="/Skills" activeClassName="selected" className="ml-2 mr-2">Skills</NavLink>
               <NavLink to="/Experience" activeClassName="selected" className="ml-2 mr-2">Experience</NavLink>
               <NavLink to="/Posts" activeClassName="selected" className="ml-2 mr-2">Posts</NavLink>
-              <NavLink to="/Projects" activeClassName="selected" className="ml-2 mr-2">Projects</NavLink>
               <NavLink to="/Contact" activeClassName="selected" className="ml-2 mr-2">Contact</NavLink>
             </Nav>
           </Navbar.Collapse>
@@ -30,11 +29,13 @@ function App() {
       <Row className="m-0 height-100">
         <Col className="col-lg-12">
           <Switch>
-            <Route path='/Posts' component={Posts} />
             <Route path='/Skills' component={Skills} />
             <Route path='/Contact' component={Contact} />
             <Route path='/Experience' component={Experience} />
-            <Route path='/Projects' component={Projects} />
+            <Route path='/Posts/1' render={() => (window.location = "https://medium.com/@jayu6766/four-years-journey-of-a-cs-undergrad-with-learning-unlearning-and-relearning-36f30540bc4a")}  />
+            <Route path='/Posts/2' render={() => (window.location = "https://medium.com/@jayu6766/journey-from-a-newbie-to-react-developer-5c7b2a8ad17f")}  />
+            <Route path='/Posts/3' render={() => (window.location = "https://medium.com/dsc-sastra-deemed-to-be-university/readme-gateway-to-open-source-project-a9afceb04384")}  />
+            <Route path='/Posts' exact component={Posts} />
             <Route path='/' exact component={About} />
           </Switch>
         </Col>
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,16 +1,72 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-
+import {Link} from 'react-router-dom';
+import { Media } from 'react-bootstrap';
+import readMe from '../assets/readme.jpeg';
+import newbie from '../assets/newbie.jpeg';
+import csUnderGrad from '../assets/csUnderGrad.jpeg';
+import './Posts.scss';
 
 export default function Posts() {
 	return (
-		<div>
-		<li><Link to='/about'>Go to Aboutpage</Link></li>
-		<li><Link to='/about'>Go to Aboutpage</Link></li>
-		<li><Link to='/about'>Go to Aboutpage</Link></li>
-		<li><Link to='/about'>Go to Aboutpage</Link></li>
-		<li><Link to='/about'>Go to Aboutpage</Link></li>
-		<li><Link to='/about'>Go to Aboutpage</Link></li>
-		</div>
+		<div className="d-flex flex-column posts">
+		<h1 className="postsTitle">Posts</h1>
+		<div className="post">
+		<Link to='/Posts/1'>
+		<Media>
+		<img
+		  width={100}
+		  height={100}
+		  className="mr-3"
+		  src={csUnderGrad}
+		  alt="Generic placeholder"
+		/>
+		<Media.Body>
+		  <h3>Four years Journey of a CS Undergrad with Learning,Unlearning and Relearning!!</h3>
+		  <p>
+		  Ultimately career road opens wide and multiple doors opens up to mold you into a precious piece..
+		  </p>
+		</Media.Body>
+	  </Media>
+		</Link>
+         </div>	 
+		 <div className="post">
+		 <Link to='/Posts/2'>
+		 <Media>
+		 <img
+		   width={100}
+		   height={100}
+		   className="mr-3"
+		   src={newbie}
+		   alt="Generic placeholder"
+		 />
+		 <Media.Body>
+		   <h3>Journey from a newbie to React developer!!</h3>
+		   <p>
+		   You are soon going to land in multi-dimensional space if you have choosen to be a developer…
+		   </p>
+		 </Media.Body>
+	   </Media>
+		 </Link>
+		  </div>	 
+		  <div className="post">
+		  <Link to='/Posts/3'>
+		  <Media>
+		  <img
+			width={100}
+			height={100}
+			className="mr-3"
+			src={readMe}
+			alt="Generic placeholder"
+		  />
+		  <Media.Body>
+			<h3>README — Gateway to Open-Source Project</h3>
+			<p>
+			Everything begins with a small amount of effort and ultimately hard-work pays off…
+			</p>
+		  </Media.Body>
+		</Media>
+		  </Link>
+		   </div>	  
+ 		</div>
 	)
 }
